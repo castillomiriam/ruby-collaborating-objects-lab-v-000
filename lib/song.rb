8b995ebd3669c3ec3f083b7d23fb3361e5add1f2
Miriam Castillo <castillo.m.mc@gmail.com>
2018-09-03 01:56:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,11 +7,9 @@ def initialize(name)
 end
 
 def self.new_by_filename(filename)
-   name = filename.split(" - ")[1]
-   song = self.new(name)
-   artist_name = filename.split(" - ")[0]
-   artist = Artist.find_or_create_by_name(artist_name)
-   artist.add_song(song)
+  song = self.new
+   song.name = filename.split(" - ")[0]
+   song.artist = filename.split(" - ")[1]
    song
  end
 
