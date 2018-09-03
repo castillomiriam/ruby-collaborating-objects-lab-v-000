b6cb63c4b23333262c2e382ef3abcbcd206122d4
Miriam Castillo <castillo.m.mc@gmail.com>
2018-09-03 01:58:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,27 +1,16 @@
 class Song
-
-  attr_accessor :name, :artist
-
-def initialize(name)
-  @name = name
-end
-
-def self.new_by_filename(filename)
-   name = filename.split(" - ")[1]
-   song = self.new(name)
-   artist_name = filename.split(" - ")[0]
-   artist = Artist.find_or_create_by_name(artist_name)
-   artist.add_song(song)
-   song
- end
-
-
-def artist_name=(name)
-  if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
+   attr_accessor :name, :artist
+   @@all = []
+   def initialize(name)
+    @name = name
   end
-
-end
+   def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+   def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
