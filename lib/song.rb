8a93a7f9d538008d5f79e23de57ab08640110acb
Miriam Castillo <castillo.m.mc@gmail.com>
2018-09-03 01:04:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,12 @@ def self.new_by_filename(filename)
 end
 
 def artist_name=(name)
+  if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
 end
 
 end
