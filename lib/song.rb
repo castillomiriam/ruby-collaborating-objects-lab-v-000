a41efb5b255314ddbf7ceb77d349d2024a582404
Miriam Castillo <castillo.m.mc@gmail.com>
2018-09-03 02:01:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,11 @@
 class Song
+  
   attr_accessor :name, :artist
+  
    def initialize(name)
     @name = name
   end
+  
    def self.new_by_filename(filename)
     song_name = filename.split(" - ")[1]
     artist_name = filename.split(" - ")[0]
@@ -11,4 +14,5 @@ class Song
     song.artist.add_song(song_name)
     song
   end
+  
 end
