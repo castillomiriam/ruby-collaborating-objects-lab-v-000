80ed7dc543ac69b620b2ad55dd248ff3a46bb528
Miriam Castillo <castillo.m.mc@gmail.com>
2018-09-03 00:16:26 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,9 +9,10 @@ class MP3Importer
   def files
   end
 
-  def import
-    Song.new_by_filename(some_filename)
+  def import(list_of_filenames)
+    list_of_filenames.each{ |filename| 
+    Song.new_by_filename(filename) }
   end
-  
+
 
 end
