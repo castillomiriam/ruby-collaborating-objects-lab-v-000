b8d8369a8966208d54ffc74028572e75f5666048
Miriam Castillo <castillo.m.mc@gmail.com>
2018-09-03 02:01:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,41 +1,33 @@
 class Artist
-
-attr_accessor :name, :songs
-
-@@all = []
-
-
-
-def initialize(name)
-  @name = name
-  @songs = []
-end
-
-def self.all
-  @@all
-end
-
-def add_song(song)
-  @songs << song
-end
-
-def save
-  @@all << self
-end
-
-def self.find_or_create_by_name(artist_name)
-  if @@all.find {|artist| artist.name == artist_name} == nil
-      new_artist = self.new(artist_name)
-      new_artist
-    else
-      @@all.find {|artist| artist.name == artist_name}
+   attr_accessor :name, :songs
+   @@all = []
+   def initialize(name)
+    @name = name
+    @songs = []
+  end
+   def self.all
+    @@all
+  end
+   def save
+    @@all << self
+  end
+   def add_song(song)
+    @songs << song
+  end
+   def self.find(name)
+    self.all.find { |artist| artist.name == name }
+  end
+   def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+   def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+   def print_songs
+    @songs.each do |song|
+      puts song.name
     end
   end
-
-def print_songs
-  self.songs.each do |song|
-    puts song.name
-  end
-end
-
-end
+ end
--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
-  
+
   attr_accessor :name, :artist
-  
+
    def initialize(name)
     @name = name
   end
-  
+
    def self.new_by_filename(filename)
     song_name = filename.split(" - ")[1]
     artist_name = filename.split(" - ")[0]
