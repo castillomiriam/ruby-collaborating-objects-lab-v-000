e61d03476f3e2b192efe1b66f98b4afd08dcd625
Miriam Castillo <castillo.m.mc@gmail.com>
2018-09-03 00:45:56 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,8 +11,8 @@ class MP3Importer
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
 end
 
-  def import(files)
-    files.each{ |filename|
+  def import
+    files.each { |filename|
     Song.new_by_filename(filename) }
   end
 
